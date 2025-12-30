b88af73c464f5258fe08f5d44643e766478d21a6
isayas habitamu <isuman3211@gmail.com>
2025-12-30 21:26:45 +0300

    Remove commented code from orders.php

--- api/orders.php
+++ api/orders.php
@@ -1,5 +1,5 @@
 <?php
-// api/orders.php
+
 declare(strict_types=1);
 
 ini_set('display_errors', '0');
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 require_once '../config/config.php';
 header('Content-Type: application/json');
 
-// $pdo is available globally
+
 
 try {
     if (!isLoggedIn()) {
